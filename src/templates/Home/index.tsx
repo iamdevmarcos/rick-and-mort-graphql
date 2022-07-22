a2a9c4b82ce5b0543109247089577bf068a7f461
Marcos Andre <marcosdev.me@gmail.com>
2022-07-22 07:23:18 -0300

    feat: implements framer motion (#15)

--- src/templates/Home/index.tsx
+++ src/templates/Home/index.tsx
@@ -1,4 +1,5 @@
 import * as S from './styles'
+import { motion } from 'framer-motion'
 
 import Logo from 'components/Logo'
 import Menu from 'components/Menu'
@@ -6,15 +7,30 @@ import Avatar from 'components/Avatar'
 
 import { HomeProps } from 'types/home'
 
+const parent = {
+  show: {
+    transition: {
+      staggerChildren: 0.4
+    }
+  }
+}
+
+const stat = {
+  hidden: { opacity: 0 },
+  show: { opacity: 1 }
+}
+
 const HomeTemplate = ({ characters }: HomeProps) => (
   <S.Wrapper>
     <Menu />
     <Logo />
 
     <S.Container>
-      <S.Grid>
+      <S.Grid variants={parent} initial="hidden" animate="show">
         {characters.map((item, index) => (
-          <Avatar key={index} {...item} />
+          <motion.div key={index} variants={stat} whileHover={{ scale: 1.1 }}>
+            <Avatar {...item} />
+          </motion.div>
         ))}
       </S.Grid>
     </S.Container>
